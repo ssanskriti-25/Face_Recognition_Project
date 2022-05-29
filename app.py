18de7ee1c2ed2c5c7be2680e0c1cbd531ff67d3e
Sanskriti Singh <87524185+ssanskriti-25@users.noreply.github.com>
2022-05-29 14:06:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-import tkinter as tk
+import tkinter as tk                           #Importing the necessary libraries
 from tkinter import filedialog
 from PIL import Image,ImageTk
 from tkinter.filedialog import askopenfile
 import os
 
-root = tk.Tk()
-canvas = tk.Canvas(root,width=1000, height=150)
+root = tk.Tk()            #Beginning of the interface
+canvas = tk.Canvas(root,width=1000, height=150)                       #To set the size of the canvas
 canvas.grid(columnspan=3, rowspan=3)
 
 #logo
@@ -15,6 +15,7 @@ logo_label = tk.Label(image=logo)
 logo_label.image = logo
 logo_label.grid(column=1 , row=0)
 
+# Defination of the main function is done
 def main():
   os.system('main.py')
   
@@ -23,7 +24,6 @@ browse_text = tk.StringVar()
 browse_btn = tk.Button(root, textvariable=browse_text, command=lambda:main(), font="Raleway", bg="#20bebe" , fg="black" , height=2, width=20, justify="center")
 browse_text.set("Take Attendance")
 browse_btn.grid(column=1, row=2)
-
 canvas = tk.Canvas(root,width=20, height=25)
 canvas.grid(columnspan=3)
-root.mainloop()
+root.mainloop()                              #End of the interface
